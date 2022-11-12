01ed6cfd77f6fae41ab4861ffd953c557c01246b
logic-b0mb-debug <bendialex72@gmail.com>
2022-11-13 02:20:11 +0300

    Update N8-itertools.py

--- N8-itertools.py
+++ N8-itertools.py
@@ -1,9 +1,9 @@
 from itertools import product  
 
-words=product('СВЕТА',repeat=5) #  с продуктом повтореятся, c пермутацией без репита 
-k=0 # присвоили каунт 
-for w in words: # честно говоря не ебу как объяснить
-    word=''.join(w) # делаем из картежей строки
-    if word.count('С')>=1: #тут просто меняем условие 
-        k+=1 # т.к счёт с нуля всегда
-print(k) # выводим
+words=product('СВЕТА',repeat=5)  
+k=0 # 
+for w in words: 
+    word=''.join(w) 
+    if word.count('С')>=1: 
+        k+=1 
+print(k) 
