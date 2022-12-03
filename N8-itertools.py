0051060226d923225ea2cd780abc8d2e1030a590
brute-f0rce <bendialex72@gmail.com>
2022-12-04 02:41:32 +0300

    Update N8-itertools.py

--- N8-itertools.py
+++ N8-itertools.py
@@ -3,7 +3,7 @@ from itertools import product
 words=product('СВЕТА',repeat=5)  
 k=0 # 
 for w in words: 
-    word=''.join(w) 
+    words=''.join(w) 
     if words.count('С')>=1: 
         k+=1 
 print(k) 
